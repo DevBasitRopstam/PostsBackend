1289cd9b3183cb2668a7e01dd4e36b99568a7780
Abdul Basit <abdul.basit@ropstam.com>
2024-06-13 12:15:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-mongoose.connect( process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-    .catch((error) => console.log(`${error} did not connect`));
+// mongoose.connect( process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+//     .catch((error) => console.log(`${error} did not connect`));
     
-    
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true });
+
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
+
 
 // MongoDB Connection
 
@@ -24,6 +26,17 @@ app.get('/', (req, res) => {
     res.send('Hello to Memories API');
 });
 
+
+
 const PORT = process.env.PORT || 4000;
+
+
+app.all("*", (req, res) => {
+    res.status(404).send("<h1>404! Page not found</h1>");
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:4000`);
+  });
 
 
