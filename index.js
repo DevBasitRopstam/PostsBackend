73208be465b9a826116f7f25b64765eb25f938f7
Abdul Basit <abdul.basit@ropstam.com>
2024-06-13 12:08:40 +0500

    code

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ import cors from 'cors';
 import postRoutes from "./routes/post.js"
 import dotenv from 'dotenv';
 
+dotenv.config();
 
+mongoose.connect( process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${error} did not connect`));
+    
+    
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
-dotenv.config();
 
 // MongoDB Connection
 
@@ -22,7 +26,4 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 
-mongoose.connect( process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-    .catch((error) => console.log(`${error} did not connect`));
-    
 
